9a6d04aebc10596b28c373cb8a4b9d128da2bb67
Lindsey Nice <nicel1@tcnj.edu>
2013-01-29 16:38:27 -0500

    Add first php file

---
+++ helloworld.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+
+	<?php
+		$txt="Hello world!";
+		$br="<br>";
+	?>
+	<title>
+		<?php echo $txt; ?>
+	</title>
+
+	<body>
+
+		<?php
+			echo $txt;
+			echo $br;	
+			echo "I'm learning to use PHP.";
+
+		?>
+
+    	<br>
+    	
+    	<?php
+    		echo "This is a very simple page.";
+    	?>
+	</body>
+</html>
